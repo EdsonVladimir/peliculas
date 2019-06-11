c287321fe5067b9a395b377a212a68aeb3cd649f
root <root@localhost.localdomain>
2019-06-11 07:29:16 -0400

    se arreglo el enviroment de produccion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  url: 'https://api.themoviedb.org/3',
+  apiKey: 'cf25ca4ed10ab9ef7719f913db395cf9',
+  imgPath: 'https://image.tmdb.org/t/p'
 };
